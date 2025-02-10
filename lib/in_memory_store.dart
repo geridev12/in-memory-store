7042c5cc8e8c235785420c6a004c77261f68f68e
Gerald <geraldnuraj@gmail.com>
2025-02-10 22:46:01 +0100

    Improved comment clarity by making messages more descriptive

--- lib/in_memory_store.dart
+++ lib/in_memory_store.dart
@@ -1,6 +1,6 @@
-/// A library for memory caching.
+/// A library for memory storing.
 ///
-/// This library provides support for creating and managing an memory cache.
+/// This library provides support for creating and managing an memory cache in a fast way./
 library;
 
 export 'src/base_in_memory_store.dart';
