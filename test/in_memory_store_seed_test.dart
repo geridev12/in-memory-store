c01ad54b0d9163eb218556e1f0dfd126dacadccd
Gerald <geraldnuraj@gmail.com>
2025-02-11 18:43:30 +0100

    Add performance comparison tests for HashMap vs Map

---
+++ test/in_memory_store_seed_test.dart
@@ -0,0 +1,62 @@
+// ignore_for_file: avoid_print, unnecessary_statements
+
+import 'dart:collection';
+import 'package:test/test.dart';
+
+void main() {
+  test(
+    'Compare HashMap vs Map performance',
+    () {
+      const iterations = 100000;
+      final normalMap = <String, int>{};
+      final hashMap = HashMap<String, int>();
+
+      final mapStopwatch = Stopwatch()..start();
+      for (var i = 0; i < iterations; i++) {
+        normalMap[r'key$i'] = i;
+      }
+      mapStopwatch.stop();
+      final mapInsertionTime = mapStopwatch.elapsedMicroseconds;
+
+      final hashMapStopwatch = Stopwatch()..start();
+      for (var i = 0; i < iterations; i++) {
+        hashMap[r'key$i'] = i;
+      }
+      hashMapStopwatch.stop();
+      final hashMapInsertionTime = hashMapStopwatch.elapsedMicroseconds;
+
+      print(r'Map insertion time: $mapInsertionTime µs');
+      print(r'HashMap insertion time: $hashMapInsertionTime µs');
+
+      expect(hashMapInsertionTime, lessThan(mapInsertionTime));
+
+      mapStopwatch
+        ..reset()
+        ..start();
+      for (var i = 0; i < iterations; i++) {
+        normalMap[r'key$i'];
+      }
+      mapStopwatch.stop();
+      final mapRetrievalTime = mapStopwatch.elapsedMicroseconds;
+
+      hashMapStopwatch
+        ..reset()
+        ..start();
+      for (var i = 0; i < iterations; i++) {
+        hashMap[r'key$i'];
+      }
+      hashMapStopwatch.stop();
+      final hashMapRetrievalTime = hashMapStopwatch.elapsedMicroseconds;
+
+      print(r'Map retrieval time: $mapRetrievalTime µs');
+      print(r'HashMap retrieval time: $hashMapRetrievalTime µs');
+
+      expect(
+        hashMapRetrievalTime,
+        lessThan(
+          mapRetrievalTime,
+        ),
+      );
+    },
+  );
+}
